86ff3fc4f1a6fd55de837f8afa019b35fdbdd40e
daeday242 <daalenadderley@yahoo.com>
2025-06-09 02:31:02 -0400

    modals updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,24 @@
 const editProfileButton = document.querySelector('.profile__edit-btn');
 const editProfileModal = document.querySelector('#edit-profile-modal');
-const newPostModal = document.querySelector('#new-post-modal');
-const closeButton = editProfileModal.querySelector('.modal__close-btn');
+const editProfileCloseButton = editProfileModal.querySelector('.modal__close-btn');
 
 editProfileButton.addEventListener('click', () => {
     editProfileModal.classList.add('modal_is-opened');
 });
 
-closeButton.addEventListener('click', () => {
+editProfileCloseButton.addEventListener('click', () => {
     editProfileModal.classList.remove('modal_is-opened');
+});
+
+
+const newPostButton = document.querySelector('.profile__add-btn');
+const newPostModal = document.querySelector('#new-post-modal');
+const newPostCloseButton = newPostModal.querySelector('.modal__close-btn');
+
+newPostButton.addEventListener('click', () => {
+    newPostModal.classList.add('modal_is-opened');
+});
+
+newPostCloseButton.addEventListener('click', () => {
+    newPostModal.classList.remove('modal_is-opened');
 });
